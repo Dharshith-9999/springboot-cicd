a6f3bb70d8395b797e2de0278b77e203139335d7
mallikarjuna <endlurimalli@gmail.com>
2026-09-07 17:45:05 +0530

    added the updated code

--- src/main/java/com/example/springboot_cicd/HelloController.java
+++ src/main/java/com/example/springboot_cicd/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello from Spring Boot CI/CD";
+    public String home() {
+        return "Hello from Spring Boot CI/CD application running successfully";
     }
 }
